2f1334e2aae2391a8dc233672b08e6bceef460ae
vitor <vitorn2009@hotmail.com>
2020-07-18 14:17:32 -0300

    Axios Component - Form - Dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
+
 function App() {
+
+  const [devs, setDevs ] = useState([])
+
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
+  },[])
+
+  async function handleAddDev(data){
+
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuario do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-          <div className="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form> 
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item" >
-            <header>
-              <img src="" alt="" />
-              <div className="user-info">
-                <strong></strong>
-                <span>ReactJS, react Native, Node.js</span>
-              </div>
-            </header>
-            <p>Full Stack Developer</p>
-            <a href="">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item" >
-            <header>
-              <img src="" alt="" />
-              <div className="user-info">
-                <strong></strong>
-                <span>ReactJS, react Native, Node.js</span>
-              </div>
-            </header>
-            <p>Full Stack Developer</p>
-            <a href="">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item" >
-            <header>
-              <img src="" alt="" />
-              <div className="user-info">
-                <strong></strong>
-                <span>ReactJS, react Native, Node.js</span>
-              </div>
-            </header>
-            <p>Full Stack Developer</p>
-            <a href="">Acessar Perfil no Github</a>
-          </li>
-          <li className="dev-item" >
-            <header>
-              <img src="" alt="" />
-              <div className="user-info">
-                <strong></strong>
-                <span>ReactJS, react Native, Node.js</span>
-              </div>
-            </header>
-            <p>Full Stack Developer</p>
-            <a href="">Acessar Perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+              <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
-
       </main>
     </div>
   );
